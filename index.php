249e7988e14c3f1a828f2f23482da71ca52a33b8
Tomasz Gora <ec2065047@edinburghcollege.ac.uk>
2023-08-01 23:50:32 +0100

    add screenshot img

--- index.php
+++ index.php
@@ -7,15 +7,13 @@
 <head>
   <title>WebFlix - Watch anything!</title>
   <link rel="icon" type="image/x-icon" href="resources/favicon.png">
-  <meta name="viewport" http-equiv="Content-Type" content="width=device-width, initial-scale=1, shrink-to-fit=no"
-    charset="utf-8">
+  <meta name="viewport" http-equiv="Content-Type" content="width=device-width, initial-scale=1, shrink-to-fit=no" charset="utf-8">
 
   <!-- JS -->
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.10.2/umd/popper.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js"></script>
-  <script type="text/javascript"
-    src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js"></script>
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js"></script>
 
   <script type="text/javascript" src="js/logged_out.js"></script>
   <script type="text/javascript" src="js/deleted.js"></script>
@@ -58,8 +56,7 @@
           <p>Goodbye!</p>
         </div>
         <div class="modal-footer">
-          <button type="button" class="btn movieCardButton overlay-play-btn" class="close" data-dismiss="modal"
-            aria-label="Close" ">Close</button>
+          <button type="button" class="btn movieCardButton overlay-play-btn" class="close" data-dismiss="modal" aria-label="Close" ">Close</button>
         </div>
       </div>
     </div>
